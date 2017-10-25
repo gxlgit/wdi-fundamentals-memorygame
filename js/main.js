8f47af9a2b17e6906504fb1fc95a4ae6960c83c2
gxlgit <gxlatasa@gmail.com>
2017-10-25 19:58:53 -0400

    created game logic

---
+++ js/main.js
@@ -0,0 +1,9 @@
+//JavaScript for GA Memory Game
+//by Gwen Latasa
+
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
